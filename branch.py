653996b57f00cf3a5fc7243d1ab23293ad8e7081
Abdul Ojulari <abdul@Abdulkabir-Tola-Ojularis-MacBook-Air.local>
2022-02-19 14:03:43 -0500

    "Initial commit"

--- branch.py
+++ branch.py
@@ -12,19 +12,30 @@ if 'master' in branch:
     else:
         print('No changes to commit! Switching to main branch')
         git.checkout('main')
-        if git.diff('main', 'origin/main'):
+        git.diff('main', 'origin/main')
+        if '' in git.diff('main', 'origin/main'):
+           print('No difference!')
+           git.rebase('master')
+           git.push('origin', 'main')
+           print('main is now up to date')
+        else:
             git.pull('origin', 'main')
             if 'fatal: refusing to merge unrelated histories' in git.pull('origin', 'main'):
                 print('You have a conflict! Resolving using --allow-unrelated-histories')
                 git.pull('origin', 'main -- allow-unrelated-histories')
             else:
                 print('Pulled from main')
-        else:
-            print('No changes to pull!')
-        git.rebase('master')
-        git.push('origin', 'main')
-        print('main is now up to date')
+
+                git.rebase('master')
+                git.push('origin', 'main')
+                if 'error: failed to push some refs to' in git.push('origin', 'main'):
+                    git.push('origin', 'main --force')
+                    print('main is now up to date')
 elif 'main' in branch:
     print('Already on main branch')
-
-
+    git.status()
+    if 'Changes not staged for commit' in git.status():
+        git.add('.')
+        git.commit('-m "update project files"')
+        git.push('origin', 'main')
+        print('Pushed to main')
