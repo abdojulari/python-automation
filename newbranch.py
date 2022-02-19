ecdcff9391d63d01e23e3df47683d15d0f475f38
Abdul Ojulari <abdul@Abdulkabir-Tola-Ojularis-MacBook-Air.local>
2022-02-19 18:51:17 -0500

    "update project files"

--- newbranch.py
+++ newbranch.py
@@ -11,18 +11,20 @@ if 'main' in git.branch('--show-current'):
     else:
         newbranch = input('Enter new branch name: ')
         git.checkout('-b', newbranch)
-        git.add('.')
-        commit = input('Enter commit message: ')
-        git.commit('-m ' + commit)
-        git.push('origin', newbranch)
-        print('Pushed to ' + newbranch)
-        
-        if newbranch in git.branch('--show-current'):
-            git.checkout('main')
-            git.merge(newbranch)
-            git.push('origin', 'main')
-            print('Merged ' + newbranch + ' into main!')
-            git.branch('-d', newbranch)
-            print('Deleted ' + newbranch)
+        if 'Changes not staged for commit' in git.status():
+            git.add('.')
+            commit = input('Enter commit message: ')
+            git.commit('-m ' + commit)
+            git.push('origin', newbranch)
+            print('Pushed to ' + newbranch)
+        else:
+            print('No changes to commit')
+    if newbranch in git.branch('--show-current'):
+        git.checkout('main')
+        git.merge(newbranch)
+        git.push('origin', 'main')
+        print('Merged ' + newbranch + ' into main!')
+        git.branch('-d', newbranch)
+        print('Deleted ' + newbranch)
 else:
     print('You are not in main branch')
