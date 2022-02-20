327de34b561ca2cc7487d16e733ef6f0f6502dac
Abdul Ojulari <abdul@Abdulkabir-Tola-Ojularis-MacBook-Air.local>
2022-02-20 07:34:28 -0500

    update newbranch

--- newbranch.py
+++ newbranch.py
@@ -4,12 +4,6 @@ import time
 
 if 'main' in git.branch('--show-current'):
     if 'Changes not staged for commit' in git.status():
-        git.add('.')
-        commit = input('Enter commit message: ')
-        git.commit('-m ' + commit)
-        git.push('origin', 'main')
-        print('Pushed to main')
-    else:
         newbranch = input('Enter new branch name: ')
         git.checkout('-b', newbranch)
         if 'Changes not staged for commit' in git.status():
@@ -18,14 +12,13 @@ if 'main' in git.branch('--show-current'):
             git.commit('-m ' + commit)
             git.push('origin', newbranch)
             print('Pushed to ' + newbranch)
-        else:
-            print('No changes to commit')
-        if newbranch in git.branch('--show-current'):
+            time.sleep(3)
             git.checkout('main')
             git.merge(newbranch)
             git.push('origin', 'main')
             print('Merged ' + newbranch + ' into main!')
-            git.branch('-d', newbranch)
-            print('Deleted ' + newbranch)
+        else:
+            print('No changes to commit')
+            
 else:
     print('You are not in main branch')
